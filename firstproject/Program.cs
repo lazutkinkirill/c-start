7ddaa3cc20fff0129bbeaf72a0270020dd68d403
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-15 18:56:05 +0300

    finish programm

---
+++ firstproject/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("write your name please: ");
+        string? username = Console.ReadLine();
+
+        if (username == "kirill")
+        {
+            Console.WriteLine("zdarova epta ");
+        }
+        else
+        {
+            Console.Write("hello, ");
+            Console.WriteLine(username);
+        }
+    }
+}
