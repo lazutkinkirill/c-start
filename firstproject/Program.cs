0867b578af7b1ac0eddb235cf60d59fc8c232d5d
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-15 19:36:01 +0300

    image

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -1,18 +1,1 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.Write("write your name please: ");
-        string? username = Console.ReadLine();
-
-        if (username == "kirill")
-        {
-            Console.WriteLine("zdarova epta ");
-        }
-        else
-        {
-            Console.Write("hello, ");
-            Console.WriteLine(username);
-        }
-    }
-}
+﻿
---
+++ secondproject/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4)
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 1,
+    xc = 1, yc = 1;
+
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("+");
+
+
+
+
